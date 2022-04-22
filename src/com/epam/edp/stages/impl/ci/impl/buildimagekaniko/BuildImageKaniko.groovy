8be257624f0c1e721c5cb97b19296920e5b13586
Yevhenii Pokhvalii <68227075+YevheniiPokhvalii@users.noreply.github.com>
2022-04-22 19:11:06 +0300

    Update BuildImageKaniko.groovy

--- src/com/epam/edp/stages/impl/ci/impl/buildimagekaniko/BuildImageKaniko.groovy
+++ src/com/epam/edp/stages/impl/ci/impl/buildimagekaniko/BuildImageKaniko.groovy
@@ -41,16 +41,11 @@ class BuildImageKaniko {
             setEnvVariable(awsCliInitContainer.env, "REPO_NAME", resultImageName, true)
             setEnvVariable(awsCliInitContainer.env, "AWS_DEFAULT_REGION", awsRegion)
 
-                // try {
                     script.withCredentials([string(credentialsId: 'edp-docker-registry', variable: 'DOCKER_REGISTRY_BASE64_CREDS')]) {
                         setEnvVariable(awsCliInitContainer.env, "DOCKER_REGISTRY_BASE64_CREDS", "${DOCKER_REGISTRY_BASE64_CREDS}")
                         script.sh(script: """
                         echo "My secret text is '${DOCKER_REGISTRY_BASE64_CREDS}'"
                         """)
-                //     }
-                // } catch (_) {
-                //     script.println("[JENKINS][DEBUG] Additional Docker Registry credentials were not provided or incorrect")
-                //     }
         }
         parsedKanikoTemplateYaml.spec.containers[0].args[0] = "--destination=${dockerRegistryHost}/${resultImageName}:${context.codebase.isTag.replaceAll("/", "-")}".toString()
         def yamlData = new Yaml().dump(parsedKanikoTemplateYaml)
